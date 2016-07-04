b1ac7acf7812ebe7991526e573cd147f07b2a138
Avis20 <orlov.avis@yandex.ru>
2016-07-04 21:08:46 +0300

    Работа со списком

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include "point.h"
+#include <list>
 
 void gotoxy(int, int);
 void Draw(int, int, char);
@@ -11,9 +12,23 @@ int main(int argc, char *argv[])
     Point p1(1, 3, '*');
     p1.Draw();
 
-
     Point p2(4, 5, '#');
     p2.Draw();
+
+    list<int> li;
+    li.push_back(0);
+    li.push_back(1);
+    li.push_back(2);
+
+    foreach (int i, li) {
+        cout << i;
+    }
+
+    li.pop_front();
+
+    list<Point> pList;
+    pList.push_back(p1);
+    pList.push_back(p2);
 
     return a.exec();
 }
